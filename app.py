5eea6a7a14750dd3b9170fbdf392e912e1d22266
Amine Fatmi <amine.fatmi.af@gmail.com>
2021-02-23 20:07:30 +0100

    Update

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import torch
 from transformers import BertForQuestionAnswering
 
 model = BertForQuestionAnswering.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
